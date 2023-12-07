3ce6d713307755975494ad675f00134bf7551279
Daniel Braaksma <danebraaksma@hotmail.com>
2023-12-07 17:03:04 -0500

    add files

---
+++ demo.js
@@ -0,0 +1,59 @@
+// ----- TEMPLATE LITERALS
+
+/* function highlight(strings, ...values) {
+  let str = '';
+  strings.forEach((string, i) => {
+    str += string;
+    if (values[i]) {
+      str += `<strong>${values[i]}</strong>`;
+    }
+  });
+  return str;
+}
+
+const name = 'Snickers';
+const age = '100';
+const sentence = highlight`My dog's name is ${name} and he is ${age} years old`;
+console.log(sentence);
+
+document.getElementById("text").innerHTML = sentence; */
+
+// ------ SCOPE REVIEW
+
+// function scope (){
+//   let y = 1;
+
+//   if (true){
+//   let y = 2;
+//   }
+
+//   console.log(y);
+// }
+
+// scope();
+
+
+
+// let hello = "hi"
+
+// function sayHi (isSpanish){
+// 	let hello = "bounjour";
+
+// 	if (isSpanish){
+// 		let hello = "hola"
+// 	return hello;
+// 	}
+// 	return hello;
+// }
+
+// console.log(hello);
+// console.log(sayHi());
+// console.log(sayHi(true));
+
+
+// DESTRUCTURING
+
+
+// ASSIGNMENT
+// create a function that accepts an object and prints out all the values into one sentence
+// Extra credit, make the function be able to handle an array or objects and print out each one
